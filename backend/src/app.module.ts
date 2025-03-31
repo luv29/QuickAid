4f3f9db70319a0e46c7b84f9dab1e8c3bf5364ff
kunj_10 <kunjvgoyal30@gmail.com>
2025-04-01 01:20:07 +0530

    Added Payments tracking

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,11 @@ import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
 import { ServiceRequestsModule } from './service-requests/service-requests.module';
 import { MechanicModule } from './mechanic/mechanic.module';
+import { PaymentServiceModule } from './payment-service/payment-service.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UsersModule, MessagesModule, ServiceRequestsModule, MechanicModule],
+  imports: [AuthModule, DatabaseModule, UsersModule, MessagesModule, ServiceRequestsModule, MechanicModule, PaymentServiceModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
