6631a0da6b51216a5c374dda3c93f4911998752d
kunj_10 <kunjvgoyal30@gmail.com>
2025-04-01 19:57:25 +0530

    Added Review Service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
-import { ServiceRequestsModule } from './service-requests/service-requests.module';
 import { MechanicModule } from './mechanic/mechanic.module';
-import { PaymentServiceModule } from './payment-service/payment-service.module';
 import { PaymentModule } from './payment/payment.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UsersModule, MessagesModule, ServiceRequestsModule, MechanicModule, PaymentServiceModule, PaymentModule],
+  imports: [AuthModule, DatabaseModule, UsersModule, MessagesModule, MechanicModule, PaymentModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/mechanic/mechanic.module.ts
+++ backend/src/mechanic/mechanic.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MechanicController } from './mechanic.controller';
 import { MechanicService } from './mechanic.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [MechanicController],
   providers: [MechanicService]
 })
